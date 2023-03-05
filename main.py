51cc444188e899cf6c39bd5e06a49055f7ebaa38
NoahFraiture <noah.fraiture@gmail.com>
2023-03-05 14:45:37 +0100

    .

--- main.py
+++ main.py
@@ -105,9 +105,9 @@ class Game:
                     if spell.name == "fire" or spell.name == "energy":
                         animations.append(Animation("fire-head_10", spell.to_player, spell.to_player.direction, size=150, speed=10, loop=2))
                     if spell.name == "ultimate":
-                        animations.append(Animation("oyster-hit_2", spell.to_player, spell.to_player.direction, size=150, speed=2, loop=1))
+                        animations.append(Animation("oyster-hit_2", spell.to_player, spell.to_player.direction, size=300, speed=2, loop=1))
                     if spell.name == "water":
-                        animations.append(Animation("water-hit", spell.to_player, spell.to_player.direction, size=150, speed=2, loop=1))
+                        animations.append(Animation("water_hit", spell.to_player, spell.to_player.direction, size=300, speed=2, loop=1))
                     if spell.apply_damage():
                         #is dead so need penguin to fall and after 3 seconds lance victory screen
                         print("you're dead")
